b18b182bd95ea0e618f4467068cc7f48b118afa6
ZMP <zane.prunella@gmail.com>
2024-03-02 14:53:47 -0800

    Redux Toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,30 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-const conuterReducer = (state = initialState, aciton) => {
-  if (aciton.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (aciton.type === "increase") {
-    return {
-      counter: state.counter + aciton.amount,
-      showCounter: state.showCounter,
-    };
-  }
-  if (aciton.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-  if (aciton.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  return state;
-};
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
-const store = createStore(conuterReducer);
+export const counterActions = counterSlice.actions;
 
 export default store;
